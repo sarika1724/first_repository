633459408ae996019efef45a20bb51dc9666833d
sarika1724 <139004863+sarika1724@users.noreply.github.com>
2023-07-08 23:28:07 +0530

    Update create_stream.py

--- create_stream.py
+++ create_stream.py
@@ -1,7 +1,8 @@
 import streamlit
 streamlit.title('My parents new healthy Diner')
 streamlit.header('My Menu')
-streamlit.text('Bluberry and oatmeal pancake')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥣Bluberry and oatmeal pancake')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Avacado toast')
 
